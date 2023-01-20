a7131584c0c933e9c7e07ec07bdb547e39da30ef
Lars Olsson <lolsson@vmware.com>
2023-01-20 01:06:58 +0100

    Update Add_Isolate_Deployments_Rule_In_NSX.py

--- aria-automation/assets/python/Add_Isolate_Deployments_Rule_In_NSX.py
+++ aria-automation/assets/python/Add_Isolate_Deployments_Rule_In_NSX.py
@@ -9,7 +9,7 @@ def handler(context, inputs):
     nsxuser = context.getSecret(inputs['nsxuser'])
     nsxpassword = context.getSecret(inputs['nsxpassword'])
     deploymentname = str(inputs["deploymentName"])
-    nsxmanager = "YOUR NSX MANAGER IP"
+    nsxmanager = "172.16.252.147:888"
 
     headers = {'content-type': 'application/json'}
 
@@ -19,18 +19,19 @@ def handler(context, inputs):
     try:
         resp = requests.patch(url=url,data=json.dumps(data),headers=headers,auth=(nsxuser,nsxpassword),verify=False)
         resp.raise_for_status()
-        print ("Created new NSX group")
+        print (f"Created new NSX group with name {deploymentname}")
     except Exception as e:
         print(f"Error: {e}")
         return
 
-    # Updating Isolate Deployment policy with a new deployment rule
+
+    # Updating Isolate Deployment policy with the new deployment
     url = f'https://{nsxmanager}/policy/api/v1/infra'
     data = { "resource_type": "Infra", "children": [ { "resource_type": "ChildDomain", "Domain": { "id": "default", "resource_type": "Domain", "children": [ { "resource_type": "ChildSecurityPolicy", "marked_for_delete": "false", "SecurityPolicy": { "id": "Isolate_deployments", "display_name": "Isolate Deployments", "resource_type": "SecurityPolicy", "category": "Environment", "sequence_number": 99, "rules": [ { "resource_type": "Rule", "display_name": f"{deploymentname}", "sources_excluded": "true", "source_groups": [ f"/infra/domains/default/groups/{deploymentname}" ], "destination_groups": [ f"/infra/domains/default/groups/{deploymentname}" ], "services": [ "ANY" ], "action": "DROP", "scope": [ f"/infra/domains/default/groups/{deploymentname}" ] } ] } } ] } } ] }
     try:
         resp = requests.patch(url=url,data=json.dumps(data),headers=headers,auth=(nsxuser,nsxpassword),verify=False)
         resp.raise_for_status()
-        print ("Updated Isolate Deployment policy with a new deployment rule")
+        print (f"Updated Isolate Deployment policy with a new rule named {deploymentname}")
     except Exception as e:
         print(f"Error: {e}")
         return
