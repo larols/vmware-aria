d6cdba6e266c61196737f9d1844df836247c0283
Lars Olsson <lolsson@vmware.com>
2023-01-27 02:21:03 +0100

    Update update_vra_tags.py

--- aria-automation/assets/python/update_vra_tags.py
+++ aria-automation/assets/python/update_vra_tags.py
@@ -6,54 +6,67 @@
 
 import requests
 import json
+import time
 
-# deploymentId = inputs['__metadata_deploymentId']
-deploymentId = "6d6caf82-1e20-46af-a8a7-03dda1e52e59"
-# resourceId = inputs['__metadata_resourceProperties']['resourceId']
-resourceId = "358a6c31-89fc-416d-a30e-a73b72f91553"
-
-newTag = "HTTPS"
-url = "YOUR VRA IP"
+newTag = "web"
 
 
 def handler(context, inputs):
     bearer_token = vraauth(inputs)
     existingTags = get_existing_tags(bearer_token, inputs)
-    print(f"Current set of tags for {resourceId} are: {existingTags}")
-    newTags = update_existingTags_with_newTag(existingTags)
-    print(f"New set of tags for {resourceId} will be: {newTags}")
+    newTags = update_existingTags_with_newTag(existingTags, inputs)
     post_updated_tags_to_resource(bearer_token, newTags, inputs)
 
 
 # post the new set of tags to the resourceId
 def post_updated_tags_to_resource(bearer_token, newTags, inputs):
+    deploymentId = inputs['__metadata_deploymentId']
+    resourceId = inputs['__metadata_resourceProperties']['resourceId']
+    url = inputs["vra_url"]
     vraheaders = {
         "accept": "application/json",
         "content-type": "application/json",
         "Authorization": "Bearer " + bearer_token
     }
+    # parse and prepare json_data
     json_data = json.dumps({
         "actionId": "Cloud.vSphere.Machine.Update.Tags",
         "inputs": {
             "tags": newTags
         },
-        "reason": "Tags updated by Day2 action"
+        "reason": "update tags"
     })
     # post new tags to resourceId
     with requests.Session() as session:
-        resp = session.post(f"{url}/deployment/api/deployments/{deploymentId}/resources/{resourceId}/requests", data=json_data, headers=vraheaders, verify=False)
-        resp.raise_for_status()
-        json_data = resp.json()
+        for i in range(3):
+            resp = session.post(f"{url}/deployment/api/deployments/{deploymentId}/resources/{resourceId}/requests",
+                                data=json_data, headers=vraheaders, verify=False)
+            if resp.status_code == 409:
+                time.sleep(15)
+                continue
+            resp.raise_for_status()
+            json_data = resp.json()
+            break
 
 
-# updating service key with new tag value
-def update_existingTags_with_newTag(existingTags):
-    newTags = [tag if tag["key"] != "service" else {"key": "service", "value": f"{newTag}"} for tag in existingTags]
-    return newTags
+# updating service key(s) with new tag value(s)
+def update_existingTags_with_newTag(existingTags, inputs):
+    deploymentId = inputs['__metadata_deploymentId']
+    resourceId = inputs['__metadata_resourceProperties']['resourceId']
+    url = inputs["vra_url"]
+    existingTags = [d for d in existingTags if d.get('key') != 'service']
+    new_services = newTag.split(", ")
+    for i, service in enumerate(new_services):
+        existingTags.append({'key': f'service', 'value': service})
+    newTags = existingTags
+    return (newTags)
 
 
 # get the existing tags for resourceId
 def get_existing_tags(bearer_token, inputs):
+    deploymentId = inputs['__metadata_deploymentId']
+    resourceId = inputs['__metadata_resourceProperties']['resourceId']
+    url = inputs["vra_url"]
     vraheaders = {
         "accept": "application/json",
         "content-type": "application/json",
@@ -64,22 +77,25 @@ def get_existing_tags(bearer_token, inputs):
         resp.raise_for_status()
         json_data = resp.json()
         existingTags = json_data["properties"]["tags"]
+        print(f"Current set of tags for {resourceId} are: {existingTags}")
         return existingTags
 
 
 # get bearer_token for vRA Api
 def vraauth(inputs):
+    url = inputs["vra_url"]
     vralogin = {
-        "username": inputs["vraUsername"],
-        "password": inputs["vraPassword"],
-        "domain": inputs['vraDomain']
+        "username": inputs["vra_username"],
+        "password": inputs["vra_password"],
+        "domain": inputs['vra_domain']
     }
     vraheaders = {
         "accept": "application/json",
         "content-type": "application/json"
     }
     with requests.Session() as session:
-        resp = session.post(f"{url}/csp/gateway/am/api/login?access_token", json=vralogin, headers=vraheaders, verify=False)
+        resp = session.post(f"{url}/csp/gateway/am/api/login?access_token", json=vralogin, headers=vraheaders,
+                            verify=False)
         resp.raise_for_status()
         refresh_token = {"refreshToken": resp.json()['refresh_token']}
         resp = session.post(f"{url}/iaas/api/login", json=refresh_token, headers=vraheaders, verify=False)
