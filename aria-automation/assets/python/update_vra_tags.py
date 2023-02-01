0f09c68cab8765a4770e9e3a78b300973c8792a6
Lars Olsson <lolsson@vmware.com>
2023-02-01 15:39:39 +0100

    Update update_vra_tags.py

--- aria-automation/assets/python/update_vra_tags.py
+++ aria-automation/assets/python/update_vra_tags.py
@@ -1,5 +1,3 @@
-#WORK IN PROGRESS - Do not expect this script to work :)
-
 # You running this script/function means you will not blame the author(s) if this breaks your stuff.
 # This script/function is provided AS IS without warranty of any kind.
 # The entire risk arising out of the use or performance of the sample scripts and documentation remains with you.
